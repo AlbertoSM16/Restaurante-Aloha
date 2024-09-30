8f12eb1e56e9851c4bdc9a470e61285e4a457c88
Alberto Siles <albertosiles2001@gmail.com>
2024-09-30 22:14:35 +0200

    probando el tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
-module.exports = {
-  content: [
-    './src/**/*.{html,js}',  // Asegúrate de que Tailwind vea tus archivos HTML y JS
-  ],
-  theme: {
-    extend: {
-      colors: {  // Corrección de 'color' a 'colors'
-        'negro-carbon': '#1B1B1D',         // Negro carbón
-        'gris-ceniza-oscuro': '#2D2D2F',   // Gris ceniza oscuro
-        'naranja-quemado': '#C75C27',      // Naranja quemado
-        'oro-envejecido': '#B08D57',       // Oro envejecido
-        'rojo-vino-profundo': '#5C1A1B',   // Rojo vino profundo
-        'verde-oliva-oscuro': '#4E5B31',   // Verde oliva oscuro
-        'blanco-humo': '#E0E0E0',          // Blanco humo
-      },
-      fontFamily: {
-        'playfair': ['Playfair Display', 'serif'],  // Fuente personalizada
+  module.exports = {
+    content: [
+      './*.{html,js}',
+      './src/**/*.{html,js}',
+      './components/**/*.{html,js}',  // Por ejemplo, si tienes una carpeta 'components' ,
+    ],
+    theme: {
+      extend: {
+        colors: {  // Corrección de 'color' a 'colors'
+          'negro-carbon': '#1B1B1D',         // Negro carbón
+          'gris-ceniza-oscuro': '#2D2D2F',   // Gris ceniza oscuro
+          'naranja-quemado': '#C75C27',      // Naranja quemado
+          'oro-envejecido': '#B08D57',       // Oro envejecido
+          'rojo-vino-profundo': '#5C1A1B',   // Rojo vino profundo
+          'verde-oliva-oscuro': '#4E5B31',   // Verde oliva oscuro
+          'blanco-humo': '#E0E0E0',          // Blanco humo
+        },
+        fontFamily: {
+          'playfair': ['Playfair Display', 'serif'],  // Fuente personalizada
+        },
       },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
