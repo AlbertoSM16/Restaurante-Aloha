f53045ec66646ca1c60883a9774f3276e11becf9
Alberto Siles <albertosiles2001@gmail.com>
2024-11-28 12:35:38 +0100

    cambios de mario

---
+++ Restaurante-Aloha/tailwind.config.js
@@ -0,0 +1,32 @@
+  module.exports = {
+    content: [
+      './*.{html,js}',
+      './src/**/*.{html,js}',
+      './components/**/*.{html,js}',  // Por ejemplo, si tienes una carpeta 'components' ,
+    ],
+    theme: {
+      extend: {
+        colors: {  // Corrección de 'color' a 'colors'
+          'negro-carbon': '#1B1B1D',         // Negro carbón
+          'gris-ceniza-oscuro': '#2D2D2F',   // Gris ceniza oscuro
+          'naranja-quemado': '#C75C27',      // Naranja quemado
+          'oro-envejecido': '#B08D57',       // Oro envejecido
+          'rojo-vino-profundo': '#5C1A1B',   // Rojo vino profundo
+          'verde-oliva-oscuro': '#4E5B31',   // Verde oliva oscuro
+          'blanco-humo': '#E0E0E0',          // Blanco humo
+          gold: {
+            DEFAULT: '#FFD700', // Dorado estándar
+            light: '#FFEC8B',  // Dorado claro
+            dark: '#B8860B',   // Dorado oscuro
+          },
+        },
+        fontFamily: {
+          'playfair': ['Playfair Display', 'serif'],  // Fuente personalizada
+        },
+        fontSize: {
+          'titulo':'40%',
+        },
+      },
+    },
+    plugins: [],
+  }
